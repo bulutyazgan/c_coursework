2ecc32a109bdbfd37b5067d0f7af2c4f3836c950
siryazgan <bulutyazgan2006@gmail.com>
2025-10-22 17:28:31 +0100

    implemented grid and robot

--- main.c
+++ main.c
@@ -1,6 +1,95 @@
+#include "graphics.h"
+
+
+#define ROWS 10
+#define COLS 10
+#define GRID_SIZE 40
+#define WINDOW_WIDTH (COLS * GRID_SIZE)
+#define WINDOW_HEIGHT (ROWS * GRID_SIZE)
+
+struct Robot {
+        int x;
+        int y;
+        int direction; // 0: up, 1: right, 2: down, 3: left
+};
+
+void drawArena();
+void drawRobot(struct Robot robot);
 
 int main(int argc, char const *argv[])
 {
+    struct Robot robot = {5, 5, 0}; //position in terms of grid coordinates, not pixels
 
+    setWindowSize(WINDOW_WIDTH + 1, WINDOW_HEIGHT + 1);
+
+    background();
+    drawArena();
+
+    foreground();
+    drawRobot(robot);
     return 0;
 }
+
+
+void drawArena(){
+    // draw grid
+    for (int r = 0; r <= ROWS; r++){
+        if(r == 0 || r == ROWS){
+            setColour(red);
+        }
+        else{
+            setColour(black);
+        }
+        drawLine(0, r * GRID_SIZE, WINDOW_WIDTH, r * GRID_SIZE);
+    }
+    for (int c = 0; c <= COLS; c++){
+        if(c == 0 || c == COLS){
+            setColour(red);
+        }
+        else{
+            setColour(black);
+        }
+        drawLine(c * GRID_SIZE, 0, c * GRID_SIZE, WINDOW_HEIGHT);
+    }
+
+}
+
+void drawRobot(struct Robot robot){
+    //triangle representing the robot
+    setColour(blue);
+    int centerX = robot.x * GRID_SIZE + GRID_SIZE / 2;
+    int centerY = robot.y * GRID_SIZE + GRID_SIZE / 2;
+    int s = GRID_SIZE / 3;
+
+    switch (robot.direction){
+        case 0: //up
+            {
+                int xPoints[3] = { centerX, centerX - s, centerX + s };
+                int yPoints[3] = { centerY - s, centerY + s, centerY + s };
+                fillPolygon(3, xPoints, yPoints);
+                break;
+            }
+        case 1: //right
+            {
+                int xPoints[3] = { centerX + s, centerX - s, centerX - s };
+                int yPoints[3] = { centerY, centerY - s, centerY + s };
+                fillPolygon(3, xPoints, yPoints);
+                break;
+            }
+        case 2: //down
+            {
+                int xPoints[3] = { centerX, centerX - s, centerX + s };
+                int yPoints[3] = { centerY + s, centerY - s, centerY - s };
+                fillPolygon(3, xPoints, yPoints);
+                break;
+            }
+        case 3: //left
+            {
+                int xPoints[3] = { centerX - s, centerX + s, centerX + s };
+                int yPoints[3] = { centerY, centerY - s, centerY + s };
+                fillPolygon(3, xPoints, yPoints);
+                break;
+            }
+    }
+}
+
